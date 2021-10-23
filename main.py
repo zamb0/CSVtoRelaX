6db9d46487f23616c0e1744822dc6283d73ce2e9
zamb0 <zamboni.fabio23@gmail.com>
2021-10-23 12:21:21 +0200

    add TODO list, implemented 'addQuotesToString' function

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-#Fabio Zamboni
-#elaboratore file csv
-#per il mitico Ferretti
+# Fabio Zamboni & Davide Ferrari
+# elaboratore file csv
+# per il mitico Ferretti
+
+
+## TODO
+## command line interface
+## parsing of csv file
+## print to a txt file
+## NULL management
 
 import csv
 from db import Db
 from table import Table
+import argparse
 
 db: Db = Db()
 
-
 def loadFile(filename: str) -> list:
     tmp: list = []
+
     with open(filename, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+
+        spamreader = csv.reader(csvfile)
+
         for row in spamreader:
+            #addQuotesToString(0, row) #<-----------   add Quotes to String element
             tmp.append(', '.join(row))
+
     return tmp
 
 def elabFile(table: list) -> Table:
@@ -29,7 +41,17 @@ def elabFile(table: list) -> Table:
             t.addTuple(items)
     return t
 
+def addQuotesToString(i: int, row: str) -> None:
+
+    i=0
+    for string in row:
+        if(string.isnumeric()):
+            pass
+        else:
+            row[i]="'"+string+"'"
+        i+=1
+
 db.addTable(elabFile(loadFile("RelazioniAlgebra/spedizioni.csv")))
 db.setGroup("Ciao")
 db.setDesctiption("Test")
-db.getDb()
+#db.getDb()
